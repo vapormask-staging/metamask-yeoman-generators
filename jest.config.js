b402be7eec89a15781e133e52cfb0379c0261a08
kumavis <aaron@kumavis.me>
2020-09-25 14:55:24 +0800

    test - update jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  moduleFileExtensions: ['ts', 'tsx', 'json', 'js', 'jsx', 'node'],
+
+  testEnvironment: 'node',
+  testTimeout: 5000,
+}
